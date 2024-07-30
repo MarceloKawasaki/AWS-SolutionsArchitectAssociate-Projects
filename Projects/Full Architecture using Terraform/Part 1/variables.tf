9462c713a217f90e24b035fededb0b4027166b54
Marcelo Kawasaki <121109924+MarceloKawasaki@users.noreply.github.com>
2024-07-30 18:02:59 -0300

    Create variables.tf

---
+++ Projects/Full Architecture using Terraform/Part 1/variables.tf
@@ -0,0 +1,7 @@
+variable vpc_cidr_block = 10.16.0.0/16
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix = A4L
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
