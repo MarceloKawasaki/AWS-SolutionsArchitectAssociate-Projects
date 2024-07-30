0a6e2edad6d313fc6d511924db98a8aef6db2746
Marcelo Kawasaki <121109924+MarceloKawasaki@users.noreply.github.com>
2024-07-30 17:56:29 -0300

    Create main.tf

---
+++ Projects/Full Architecture using Terraform/Part 1/main.tf
@@ -0,0 +1,138 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+#Variables created
+#terraform.tfvars separate file to define variable values, but not on the GitHub Repository
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
+
+#Creating the AWS VPC
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+#Creating the Subnet for the VPC
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-subnet-1"
+    }
+}
+
+#Creating the Route Table and assigning the Internet Gateway
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id #Internet Gateway
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+}
+
+#Creating the Internet Gateway
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
+}
+
+#Creating Route Table Subnet Association
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+}
+
+
+#Creating Security Group
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    #Define inbound rules for incoming traffic (FireWall) for port 22
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
+
+    #Define inbound rules for incoming traffic (FireWall) for port 8080
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    #Define outbound rules for outgoing traffic (FireWall) for all
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-sg"
+    }
+}
+
+
+#Fetching data from AWS to retrieve updated AMIs
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+#Automate Key Pair Creation
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
+#Creating EC2 
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    #Associate public IP
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = file("entry-script.sh")
+
+    user_data_replace_on_change =  true
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    } 
+}
